dffe36fd1b1a1dd1eb0dea42eb53784b4c708ce1
TM-Code-009 <osakweterrynduka@gmail.com>
2024-06-25 02:43:22 +0100

    mistake amended

--- app/api/(auth)/register/[companyID]/route.ts
+++ app/api/(auth)/register/[companyID]/route.ts
@@ -1,42 +1,93 @@
-import { Schema, Types, model, models } from "mongoose";
-import { iStaffData } from "../../../../utils/interface.i";
+import { dbConfig } from "../../../../utils/dbconfig";
+import companyData from "../../../../utils/model/companymodel";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import staffData from "../../../../utils/model/staffmodel";
+import { Types } from "mongoose";
 
-const staffModel = new Schema<iStaffData>(
-  {
-    staffName: {
-      type: String,
-    },
-    email: {
-      type: String,
-      unique: true,
-    },
-    password: {
-      type: String,
-    },
-    address: {
-      type: String,
-    },
-    role: {
-      type: String,
-    },
-    avatar: {
-      type: String,
-    },
-    company: {
-      type: Types.ObjectId,
-      ref: "Companies",
-    },
+export const POST = async (req: NextRequest, { params }: any) => {
+  try {
+    const { companyID } = params;
+    const { staffName } = await req.json();
+    await dbConfig();
 
-    steps: [
-      {
-        type: Types.ObjectId,
-        ref: "Steps",
+    const companyDetail = await companyData.findById(companyID);
+    //peteroti@company.com
+    let email = staffName
+      .replaceAll(" ", "")
+      .concat("@", `${companyDetail.companyName}.com`)
+      .toLowerCase();
+
+    const password = staffName.toLowerCase().replaceAll(" ", "");
+
+    console.log(companyDetail);
+
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(password, salt);
+
+    if (companyDetail.plan === "Freemium") {
+      if (companyDetail.staff.length < 3) {
+        const staff = await staffData.create({
+          role: "staff",
+          email,
+          password: hashed,
+          staffName,
+        });
+
+        const myCompany = await companyDetail.staff.push(
+          new Types.ObjectId(staff._id)
+        );
+        companyDetail.save();
+
+        return NextResponse.json({
+          message: "company staff created",
+          data: { myCompany, staff },
+          status: 201,
+        });
+      } else {
+        return NextResponse.json({
+          message: "Upgrade your plan for more staff",
+
+          status: 404,
+        });
+      }
+    } else if (companyDetail.plan === "starter") {
+    } else {
+    }
+  } catch (error: any) {
+    return NextResponse.json({
+      message: "Error getting companies",
+      error: error.message,
+      status: 404,
+    });
+  }
+};
+
+export const GET = async (req: NextRequest, { params }: any) => {
+  try {
+    await dbConfig();
+    const { companyID } = params;
+    await dbConfig();
+
+    const companyDetail = await companyData.findById(companyID).populate({
+      path: "staff",
+      options: {
+        sort: {
+          createdAt: -1,
+        },
       },
-    ],
-  },
-  { timestamps: true }
-);
+    });
 
-const staffData = models.Staffs || model<iStaffData>("Staffs", staffModel);
-
-export default staffData;
+    return NextResponse.json({
+      message: "Getting company's staffs",
+      data: companyDetail,
+      status: 201,
+    });
+  } catch (error: any) {
+    return NextResponse.json({
+      message: "Error getting companies",
+      error: error.message,
+      status: 404,
+    });
+  }
+};
--- app/api/(auth)/signin/route.ts
+++ app/api/(auth)/signin/route.ts
@@ -1,7 +1,7 @@
 
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcrypt";
-import staffData from "../register/[companyID]/route";
+import staffData from "../../../utils/model/staffmodel";
 import companyData from "@/app/utils/model/companymodel";
 import { dbConfig } from "@/app/utils/dbconfig";
 
