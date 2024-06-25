f3c4ab328df452db44fc9402cab636e4e8524d1d
TM-Code-009 <osakweterrynduka@gmail.com>
2024-06-25 07:07:29 +0100

    using axios for for server fetching

--- app/(Auth)/Register/[reg]/page.tsx
+++ app/(Auth)/Register/[reg]/page.tsx
@@ -1,16 +1,17 @@
-import { URL } from "@/app/utils/constant";
-import { data } from "@/app/utils/data";
+import { URL } from "../../../utils/constant";
+import { data } from "../../../utils/data";
 import Link from "next/link";
 import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa6";
+import axios from 'axios'
 
-const page = async ({params}:any) => {
+const page = async ({ params }: any) => {
   const { reg } = params;
   const readData = data;
-  // console.log(`readinvg reg: ${reg}`)
 
   const plan = reg.toString().charAt(0).toUpperCase().concat(reg.substring(1));
+
   console.log(plan);
   console.log(
     "data: ",
@@ -23,105 +24,110 @@ const page = async ({params}:any) => {
     return el.name === plan;
   });
 
+  const createAccount = async (data: FormData) => {
+    "use server";
+    const companyName = data.get("companyName") as string;
+    const email = data.get("email") as string;
+    const password = data.get("password") as string;
 
-    //const {reg} = params
 
-    console.log("this is " + reg + "Screen");
-    
-    const createAccount = async (data: FormData) => {
-      "use server";
-      const companyName = data.get("companyName") as string;
-      const email = data.get("email") as string;
-      const password = data.get("password") as string;
-  
-      await fetch(`${URL}/api/register`, {
-        method: "POST",
+
+    try {
+      const response = await axios.post(`${URL}/api/register`, {
+        companyName,
+        email,
+        password,
+        plan,
+        planCost: cost?.price === "Free" ? 0 : parseInt(cost?.price),
+      }, {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          companyName,
-          email,
-          password,
-          plan,
-          planCost: cost?.price === "Free" ? 0 : parseInt(cost?.price),
-        }),
       });
-    };
+  
+      console.log('Response data:', response.data);
+      // Handle response data as needed
+  
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Handle error as needed
+    }
+  };
+  
   return (
     <div className="flex w-full h-screen justify-center items-center">
-    <div className="border rounded-md w-[500px] min-h-[300px] p-4 ">
-      <div>Register Screen for {plan}</div>
+      <div className="border rounded-md w-[500px] min-h-[300px] p-4 ">
+        <div>Register Screen for {plan}</div>
 
-      <div className="my-10">
-        <hr />
-      </div>
-
-      <form action={createAccount}>
-        <div className="flex flex-col mb-4">
-          <label className="font-semibold text-[12px]">Company Name</label>
-          <input
-            type="text"
-            name="companyName"
-            placeholder="Display Name"
-            className="border outline-none h-[45px] rounded-md pl-2"
-          />
-        </div>
-        <div className="flex flex-col mb-4">
-          <label className="font-semibold text-[12px]">Email</label>
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            className="border outline-none h-[45px] rounded-md pl-2"
-          />
-        </div>
-        <div className="flex flex-col mb-4">
-          <label className="font-semibold text-[12px]">Password</label>
-          <input
-            type="text"
-            name="password"
-            placeholder="Password"
-            className="border outline-none h-[45px] rounded-md pl-2"
-          />
+        <div className="my-10">
+          <hr />
         </div>
 
-        <button
-          type="submit"
-          className="w-full mt-8 h-[55px] flex items-center justify-center text-white bg-neutral-800 rounded-md"
-        >
-          Register
-        </button>
-      </form>
+        <form action={createAccount}>
+          <div className="flex flex-col mb-4">
+            <label className="font-semibold text-[12px]">Company Name</label>
+            <input
+              type="text"
+              name="companyName"
+              placeholder="Display Name"
+              className="border outline-none h-[45px] rounded-md pl-2"
+            />
+          </div>
+          <div className="flex flex-col mb-4">
+            <label className="font-semibold text-[12px]">Email</label>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              className="border outline-none h-[45px] rounded-md pl-2"
+            />
+          </div>
+          <div className="flex flex-col mb-4">
+            <label className="font-semibold text-[12px]">Password</label>
+            <input
+              type="text"
+              name="password"
+              placeholder="Password"
+              className="border outline-none h-[45px] rounded-md pl-2"
+            />
+          </div>
 
-      <div className="text-[12px] my-4 text-center  ">
-        Already have an Account{" "}
-        <Link href="./Signin" className="italic font-semibold ">
-          Sign in Here
-        </Link>
-      </div>
-
-      <div className="my-3">
-        <hr />
-      </div>
-
-      <div>
-        <p className="text-[12px] font-bold">
-          Register through your Social Account
-        </p>
-
-        <div className="flex items-center gap-2">
-          <button className="w-full mt-2 h-[55px] flex items-center justify-center text-white bg-red-600 hover:bg-red-500 duration-300  rounded-md text-[35px]">
-            <FaGoogle />
+          <button
+            type="submit"
+            className="w-full mt-8 h-[55px] flex items-center justify-center text-white bg-neutral-800 rounded-md"
+          >
+            Register
           </button>
-          <button className="w-full mt-2 h-[55px] flex items-center justify-center text-white bg-neutral-800 hover:bg-neutral-900 rounded-md text-[35px]">
-            <FaGithub />
-          </button>
+        </form>
+
+        <div className="text-[12px] my-4 text-center  ">
+          Already have an Account{" "}
+          <Link href="/signin" className="italic font-semibold ">
+            Sign in Here
+          </Link>
+        </div>
+
+        <div className="my-3">
+          <hr />
+        </div>
+
+        <div>
+          <p className="text-[12px] font-bold">
+            Register through your Social Account
+          </p>
+
+          <div className="flex items-center gap-2">
+            <button className="w-full mt-2 h-[55px] flex items-center justify-center text-white bg-red-600 hover:bg-red-500 duration-300  rounded-md text-[35px]">
+              <FaGoogle />
+            </button>
+            <button className="w-full mt-2 h-[55px] flex items-center justify-center text-white bg-neutral-800 hover:bg-neutral-900 rounded-md text-[35px]">
+              <FaGithub />
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
-};
+}
 
 export default page;
